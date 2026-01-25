0b8dd32eb82bf99dcb20ab9cc96fdde47863b5eb
Lorenzo Lopez <lloonalo1809@gmail.com>
2026-01-25 10:27:22 -0300

    Add files via upload

---
+++ paginas_webs/TiendaOnline/inicio.js
@@ -0,0 +1,19 @@
+/* --------------------------------- CARRUSEL 1 -------------------------------*/
+
+var swiper = new Swiper(".mySwiper-1", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
